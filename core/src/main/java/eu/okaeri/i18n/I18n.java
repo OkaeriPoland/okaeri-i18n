e31d9d4200833c31665807c6e804d66c7ad73bd8
Sandra <sandra@okaeri.cloud>
2021-04-08 10:10:05 +0200

    Update to new LocaleProvider schema, release 1.1.0

--- core/src/main/java/eu/okaeri/i18n/I18n.java
+++ core/src/main/java/eu/okaeri/i18n/I18n.java
@@ -5,7 +5,8 @@ import eu.okaeri.i18n.provider.LocaleProvider;
 import java.util.Locale;
 
 public interface I18n<K, M> {
-    <T> I18n registerLocaleProvider(Class<T> entityType, LocaleProvider<T> localeProvider);
+    <T> I18n registerLocaleProvider(LocaleProvider<T> localeProvider);
+    <T> I18n registerLocaleProvider(Class<?> overrideType, LocaleProvider<T> localeProvider);
     Locale getDefaultLocale();
     LocaleProvider<?> getLocaleProvider(Class<?> entityType);
     Locale getLocale(Object entity);
