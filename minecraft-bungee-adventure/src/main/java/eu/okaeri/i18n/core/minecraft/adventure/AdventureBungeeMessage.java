839ec72f6ee14d883fada7e46d9205d5c13b4931
PinkLolicorn <pinklolicorngithub@daffit.pl>
2023-01-16 14:30:03 +0100

    Rename minecraft-adventure-bungee to minecraft-bungee-adventure and add common super

--- minecraft-bungee-adventure/src/main/java/eu/okaeri/i18n/core/minecraft/adventure/AdventureBungeeMessage.java
+++ minecraft-bungee-adventure/src/main/java/eu/okaeri/i18n/core/minecraft/adventure/AdventureBungeeMessage.java
@@ -1,34 +1,45 @@
 package eu.okaeri.i18n.core.minecraft.adventure;
 
-import eu.okaeri.i18n.message.Message;
 import eu.okaeri.placeholders.Placeholders;
 import eu.okaeri.placeholders.message.CompiledMessage;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 
-import java.awt.*;
 
-@RequiredArgsConstructor
-public class BungeeAdventureMessage implements Message {
+public class AdventureBungeeMessage extends BungeeMessage {
 
     private static final GsonComponentSerializer GSON_SERIALIZER = GsonComponentSerializer.gson();
 
     @Delegate(excludes = Overrides.class) private final AdventureMessage adventure;
 
-    public static BungeeAdventureMessage of(Placeholders placeholders, @NonNull CompiledMessage compiled) {
-        return new BungeeAdventureMessage(AdventureMessage.of(placeholders, compiled));
+    protected AdventureBungeeMessage(@NonNull AdventureMessage message) {
+        super(message.compiled(), message.context());
+        this.adventure = message;
+    }
+
+    public static AdventureBungeeMessage of(Placeholders placeholders, @NonNull CompiledMessage compiled) {
+        return new AdventureBungeeMessage(AdventureMessage.of(placeholders, compiled));
     }
 
     interface Overrides {
         Component component();
     }
 
+    @Override
     public BaseComponent[] component() {
         String json = GSON_SERIALIZER.serialize(this.adventure.component());
         return ComponentSerializer.parse(json);
+    }
+
+    /**
+     * @deprecated Unstable hack API.
+     */
+    @Deprecated
+    public Component adventure() {
+        return this.adventure.component();
     }
 }
---
+++ minecraft-bungee/src/main/java/eu/okaeri/i18n/core/minecraft/adventure/BungeeMessage.java
@@ -0,0 +1,18 @@
+package eu.okaeri.i18n.core.minecraft.adventure;
+
+import eu.okaeri.i18n.message.SimpleMessage;
+import eu.okaeri.placeholders.context.PlaceholderContext;
+import eu.okaeri.placeholders.message.CompiledMessage;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class BungeeMessage extends SimpleMessage {
+
+    protected BungeeMessage(CompiledMessage compiled, PlaceholderContext context) {
+        super(compiled, context);
+    }
+
+    public BaseComponent[] component() {
+        return TextComponent.fromLegacyText(this.apply());
+    }
+}
